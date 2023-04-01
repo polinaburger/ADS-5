bfc1aea1818d3bd1c9be8686f471325eedaa67e2
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-04-01 18:32:02 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,37 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
-class TStack {
-  // добавьте код стека
+template<typename type, int size>
+    class TStack {
+ private:
+    type* arr;
+    int top;
+ public:
+    TStack() {
+        arr = new type[size];
+        top = -1;
+    }
+    const type& get() {
+    if (isEmpty()) {
+      throw std::string("empty");
+    } else {
+      return arr[top];
+    }
+  }
+    bool isEmpty() const {
+        return top==-1
+    }
+    bool isFull() const {
+        return top == size -1;
+    }
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+    void push(type item) {
+        if(top <size-1)
+            arr[++top] = item;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
